c8ae8630e38bc2f75d449d0f933488c53522212a
Teona <tekizashvili@gmail.com>
2023-05-09 21:15:22 +0400

    added cart button component

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -1,4 +1,78 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { FaShoppingCart, FaUserPlus } from 'react-icons/fa';
+
 const CartButtons = () => {
-  return <div>CartButtons</div>;
+  return (
+    <Wrapper className="cart-btn-wrapper">
+      <Link to="/cart" className="cart-btn">
+        Cart
+        <span className="cart-container">
+          <FaShoppingCart />
+          <span className="cart-value">12</span>
+        </span>
+      </Link>
+      <button className="auth-btn">
+        Login
+        <FaUserPlus />
+      </button>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  width: 225px;
+
+  .cart-btn {
+    color: var(--clr-grey-1);
+    font-size: 1.3rem;
+    letter-spacing: var(--spacing);
+    display: flex;
+    align-items: center;
+  }
+
+  .cart-container {
+    display: flex;
+    align-items: center;
+    position: relative;
+    svg {
+      height: 1.6rem;
+      margin-left: 5px;
+    }
+  }
+
+  .cart-value {
+    position: absolute;
+    top: -6px;
+    right: -15px;
+    background: var(--clr-primary-5);
+    width: 16px;
+    height: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    font-size: 0.7rem;
+    color: var(--clr-white);
+    padding: 10px;
+  }
+
+  .auth-btn {
+    display: flex;
+    align-items: center;
+    background: transparent;
+    border-color: transparent;
+    font-size: 1.3rem;
+    cursor: pointer;
+    color: var(--clr-grey-1);
+    letter-spacing: var(--spacing);
+    svg {
+      margin-left: 5px;
+    }
+  }
+`;
+
 export default CartButtons;
