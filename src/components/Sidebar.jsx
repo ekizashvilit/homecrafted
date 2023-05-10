cdf39ae60c03dc8b26e7afc4b5d72fec230695ab
Teona <tekizashvili@gmail.com>
2023-05-10 09:44:51 +0400

    added and styled sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,116 @@
+import styled from 'styled-components';
+import logo from '../assets/logo.svg';
+import { FaTimes } from 'react-icons/fa';
+import { links } from '../utils/constants';
+import { Link } from 'react-router-dom';
+import CartButtons from './CartButtons';
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const isOpen = true;
+  return (
+    <SidebarContainer>
+      <aside className={isOpen ? 'sidebar show-sidebar' : 'sidebar'}>
+        <div className="sidebar-header">
+          <img src={logo} alt="homecrafted" className="logo" />
+          <button className="close-btn">
+            <FaTimes />
+          </button>
+        </div>
+        <ul className="links">
+          {links.map(({ id, text, url }) => {
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            );
+          })}
+          <li>
+            <Link to="/checkout">checkout</Link>
+          </li>
+        </ul>
+        <CartButtons />
+      </aside>
+    </SidebarContainer>
+  );
 };
+
+const SidebarContainer = styled.div`
+  text-align: center;
+
+  .sidebar-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 1.5rem;
+  }
+
+  .close-btn {
+    font-size: 1.5rem;
+    background: transparent;
+    border-color: transparent;
+    transition: var(--transition);
+    cursor: pointer;
+    color: var(--clr-red-dark);
+    margin-top: 0.2rem;
+  }
+
+  .close-btn:hover {
+    color: var(--clr-red-light);
+  }
+
+  .logo {
+    justify-self: center;
+    height: 60px;
+  }
+
+  .links {
+    margin-bottom: 2rem;
+  }
+
+  .links a {
+    display: block;
+    text-align: left;
+    font-size: 1rem;
+    text-transform: capitalize;
+    padding: 0.8rem 1.5rem;
+    color: var(--clr-grey-3);
+    transition: var(--transition);
+    letter-spacing: var(--spacing);
+  }
+
+  .links a:hover {
+    padding: 0.8rem 1.5rem;
+    padding-left: 2rem;
+    background: var(--clr-grey-10);
+    color: var(--clr-grey-2);
+  }
+
+  .sidebar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: var(--clr-white);
+    transition: var(--transition);
+    transform: translate(-100%);
+    z-index: -1;
+  }
+
+  .show-sidebar {
+    transform: translate(0);
+    z-index: 999;
+  }
+
+  .cart-btn-wrapper {
+    margin: 2rem auto;
+  }
+
+  @media (min-width: 992px) {
+    .sidebar {
+      display: none;
+    }
+  }
+`;
+
 export default Sidebar;
