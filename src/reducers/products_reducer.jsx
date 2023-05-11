ba7d2d1cb21557d64d085808304f3a5f49062a81
Teona <tekizashvili@gmail.com>
2023-05-11 19:16:02 +0400

    fetch single product

--- src/reducers/products_reducer.jsx
+++ src/reducers/products_reducer.jsx
@@ -4,6 +4,9 @@ import {
   GET_PRODUCTS_BEGIN,
   GET_PRODUCTS_SUCCESS,
   GET_PRODUCTS_ERROR,
+  GET_SINGLE_PRODUCT_BEGIN,
+  GET_SINGLE_PRODUCT_SUCCESS,
+  GET_SINGLE_PRODUCT_ERROR,
 } from '../actions';
 
 const products_reducer = (state, action) => {
@@ -33,6 +36,26 @@ const products_reducer = (state, action) => {
   }
 
   if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_error: true, products_loading: false };
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state,
+      single_product_loading: true,
+      single_product_error: false,
+    };
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product: action.payload,
+    };
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
     return { ...state, products_error: true, products_loading: false };
   }
 
