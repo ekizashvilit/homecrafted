5a5395187bcf7c1c454b564cb8b9b23c4f9909ec
Teona <tekizashvili@gmail.com>
2023-05-11 10:18:33 +0400

    added fetch products functionality

--- src/context/products_context.jsx
+++ src/context/products_context.jsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import { createContext, useContext, useEffect, useReducer } from 'react';
 import reducer from '../reducers/products_reducer';
-import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from '../actions';
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+} from '../actions';
+
+const url = 'https://course-api.com/react-store-products';
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 };
 
 const ProductsContext = createContext();
@@ -18,6 +31,22 @@ export const ProductsProvider = ({ children }) => {
   const closeSidebar = () => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
+
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+
+    try {
+      const response = await axios(url);
+      const products = response.data;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts(url);
+  }, []);
 
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
