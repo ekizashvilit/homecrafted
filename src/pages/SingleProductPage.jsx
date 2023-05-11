e6f0fb1bbe6e05fa4704e6eb284fdbac80f38eb1
Teona <tekizashvili@gmail.com>
2023-05-11 22:18:31 +0400

    single product - loading, error

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,4 +1,40 @@
+import { useParams, useNavigate } from 'react-router-dom';
+import { useProductsContext } from '../context/products_context';
+import { Error, Loading } from '../components';
+import { useEffect } from 'react';
+
+const url = `https://course-api.com/react-store-single-product?id=`;
+
 const SingleProductPage = () => {
+  const { id } = useParams();
+  const {
+    single_product_loading: loading,
+    single_product_error: error,
+    single_product: product,
+    fetchSingleProduct,
+  } = useProductsContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`);
+  }, [id]);
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        navigate('/');
+      }, 5000);
+    }
+  }, [error]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+
   return <div>SingleProductPage</div>;
 };
 export default SingleProductPage;
--- src/reducers/products_reducer.jsx
+++ src/reducers/products_reducer.jsx
@@ -56,7 +56,11 @@ const products_reducer = (state, action) => {
   }
 
   if (action.type === GET_SINGLE_PRODUCT_ERROR) {
-    return { ...state, products_error: true, products_loading: false };
+    return {
+      ...state,
+      single_product_error: true,
+      single_product_loading: false,
+    };
   }
 
   throw new Error(`No Matching "${action.type}" - action type`);
