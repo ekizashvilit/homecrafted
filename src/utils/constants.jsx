437dfe9182fa823d8386fefae237625d5f733c47
Teona <tekizashvili@gmail.com>
2023-05-11 09:17:24 +0400

    home page - contact

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -23,18 +23,18 @@ export const services = [
     id: 1,
     icon: <GiCompass />,
     title: 'mission',
-    text: `Our mission is to create furniture that not only meets but exceeds our customers' expectations. We are committed to using only the highest quality materials and working with skilled craftsmen to create unique, custom pieces that reflect the individuality of each customer.`,
+    text: `Our mission is to create furniture that not only meets but exceeds our customers' expectations. We are committed to using only the highest quality materials and working with skilled craftsmen.`,
   },
   {
     id: 2,
     icon: <GiDiamondHard />,
     title: 'vision',
-    text: `Our vision is to be the leading provider of custom-made furniture, delivering exceptional quality and unparalleled customer service. We strive to exceed our customers' expectations by creating unique pieces that reflect their personal style and enhance the beauty and functionality of their homes.`,
+    text: `Our vision is to be the leading provider of custom-made furniture, delivering exceptional quality and unparalleled customer service.`,
   },
   {
     id: 3,
     icon: <GiStabbedNote />,
     title: 'history',
-    text: `Our history is a story of passion and craftsmanship. From our humble beginnings as a small furniture shop, we have grown into a trusted provider of high-quality custom furniture. We take pride in our heritage and the relationships we have built with our customers over the years.`,
+    text: `Our history is a story of passion and craftsmanship. From our humble beginnings as a small furniture shop, we have grown into a trusted provider of high-quality custom furniture.`,
   },
 ];
