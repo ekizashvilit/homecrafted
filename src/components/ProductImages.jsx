10e3cc0d9e7cd99dd9f71228ea0791732b217d1f
Teona <tekizashvili@gmail.com>
2023-05-13 22:53:58 +0400

    single product stars, manual approach

--- src/components/ProductImages.jsx
+++ src/components/ProductImages.jsx
@@ -4,8 +4,6 @@ import styled from 'styled-components';
 const ProductImages = ({ images = [{ url: '' }] }) => {
   const [main, setMain] = useState(images[0]);
 
-  // console.log(images);
-
   return (
     <Wrapper>
       <img src={main.url} alt={main.filename} className="main" />
