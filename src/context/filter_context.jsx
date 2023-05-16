cdcaa81edb34f40f2a045f16b209d3f566cc62f2
Teona <tekizashvili@gmail.com>
2023-05-16 08:52:45 +0400

    filter context setup

---
+++ src/context/filter_context.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useEffect, useReducer } from 'react';
+import {
+  LOAD_PRODUCTS,
+  SET_GRIDVIEW,
+  SET_LISTVIEW,
+  UPDATE_SORT,
+  SORT_PRODUCTS,
+  UPDATE_FILTERS,
+  FILTER_PRODUCTS,
+  CLEAR_FILTERS,
+} from '../actions';
+import reducer from '../reducers/filter_reducer';
+import { useProductsContext } from './products_context';
+
+const initialState = {
+  filtered_products: [],
+  all_products: [],
+};
+
+const FilterContext = createContext();
+
+export const FilterProvider = ({ children }) => {
+  const { products } = useProductsContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products });
+  }, [products]);
+
+  return <FilterContext.Provider value="hi">{children}</FilterContext.Provider>;
+};
+
+export const useFilterContext = () => {
+  return useContext(FilterContext);
+};
