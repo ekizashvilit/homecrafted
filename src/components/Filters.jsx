0d4bec261b157d9d08513ecdd85f399e59b526bd
Teona <tekizashvili@gmail.com>
2023-05-17 09:03:58 +0400

    set up filters search functionality

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,4 +1,43 @@
+import styled from 'styled-components';
+import { useFilterContext } from '../context/filter_context';
+
 const Filters = () => {
-  return <div>Filters</div>;
+  const {
+    updateFilters,
+    clearFilters,
+    all_products,
+    filters: {
+      text,
+      company,
+      category,
+      color,
+      min_price,
+      max_price,
+      price,
+      shipping,
+    },
+  } = useFilterContext();
+
+  return (
+    <Wrapper>
+      <div className="content">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="search"
+              className="search-input"
+              name="text"
+              value={text}
+              onChange={updateFilters}
+            />
+          </div>
+        </form>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section``;
+
 export default Filters;
