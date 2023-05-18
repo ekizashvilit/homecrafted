8c79c872a19f4e48c004ffa0bb19b47e45dbdb2b
Teona <tekizashvili@gmail.com>
2023-05-18 11:43:59 +0400

    added shipping and clear cart functionality

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -120,7 +120,20 @@ const Filters = () => {
               value={price}
             />
           </div>
+          <div className="form-control shipping">
+            <label htmlFor="shipping">free shipping</label>
+            <input
+              type="checkbox"
+              name="shipping"
+              id="shipping"
+              onChange={updateFilters}
+              checked={shipping}
+            />
+          </div>
         </form>
+        <button className="clear-btn" onClick={clearFilters}>
+          clear filters
+        </button>
       </div>
     </Wrapper>
   );
