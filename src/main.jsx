ce22940627daea0f546719c7f533bd80cef51f37
Teona <tekizashvili@gmail.com>
2023-05-20 11:20:12 +0400

    auth0 setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,25 @@ import './index.css';
 import { ProductsProvider } from './context/products_context.jsx';
 import { FilterProvider } from './context/filter_context.jsx';
 import { CartProvider } from './context/cart_context.jsx';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+// DOMAIN - dev-tavdtsbl4lprrlxw.us.auth0.com
+// ID - UnlzL1x12vuyAScqru7t7qjJDZthwoEy
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="dev-tavdtsbl4lprrlxw.us.auth0.com"
+    clientId="UnlzL1x12vuyAScqru7t7qjJDZthwoEy"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>
 );
