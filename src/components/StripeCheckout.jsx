a57fdc24779b0e242b79d5701925b5fbc64e5ed6
Teona <tekizashvili@gmail.com>
2023-05-24 13:18:33 +0400

    stripe checkout imports

--- src/components/StripeCheckout.jsx
+++ src/components/StripeCheckout.jsx
@@ -1,4 +1,18 @@
 import styled from 'styled-components';
+import { loadStripe } from '@stripe/stripe-js';
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements,
+} from '@stripe/react-stripe-js';
+import axios from 'axios';
+import { useCartContext } from '../context/cart_context';
+import { useUserContext } from '../context/user_context';
+import { formatPrice } from '../utils/helpers';
+import { useNavigate } from 'react-router-dom';
+
+const promise = loadStripe(import.meta.env.VITE_APP_STRIPE_PUBLIC_KEY);
 
 const CheckoutForm = () => {
   return <h1>hi from checkout</h1>;
@@ -7,7 +21,9 @@ const CheckoutForm = () => {
 const StripeCheckout = () => {
   return (
     <Wrapper>
-      <CheckoutForm />
+      <Elements stripe={promise}>
+        <CheckoutForm />
+      </Elements>
     </Wrapper>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -13,8 +13,8 @@ import { UserProvider } from './context/user_context.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
-    domain="dev-tavdtsbl4lprrlxw.us.auth0.com"
-    clientId="UnlzL1x12vuyAScqru7t7qjJDZthwoEy"
+    domain={import.meta.env.VITE_APP_AUTH_DOMAIN}
+    clientId={import.meta.env.VITE_APP_AUTH_CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
