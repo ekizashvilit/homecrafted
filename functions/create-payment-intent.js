fac85bf016cfbcda2a01e1a9f5d4edf1e82c37dc
Teona <tekizashvili@gmail.com>
2023-05-25 09:53:45 +0400

    get client secret

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,16 +1,44 @@
-// /.netlify/functions/create-payment-intent
+require('dotenv').config();
+
+const stripe = require('stripe')(process.env.VITE_APP_STRIPE_SECRET_KEY);
 
 exports.handler = async function(event, context) {
   if (event.body) {
     const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(cart),
+    const calculateOrderAmount = () => {
+      return shipping_fee + total_amount;
     };
+
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(),
+        currency: 'usd',
+      });
+
+      return {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ msg: error.message }),
+      };
+    }
   }
+
   return {
     statusCode: 200,
-    body: 'create payment intent',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify('create payment intent'),
   };
 };
